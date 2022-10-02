e8e207bcc5fc9ff35729be354a8facfbe225a772
Avin <avinindrasoma@gmail.com>
2022-10-02 14:54:15 +0530

    Corrected links to lead to new resume

--- src/views/About.vue
+++ src/views/About.vue
@@ -27,7 +27,7 @@
                         </li>
                         <li>
                             Download my
-                            <a href="./Iuri-de-Paula-Resume.pdf" target="_blank" title="Download Resume" class="bt">
+                            <a href="./resume.pdf" target="_blank" title="Download Resume" class="bt">
                                 resume <svg xmlns="http://www.w3.org/2000/svg" class="ico" viewBox="0 0 16 16"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M8 11.4l3.3-2.9m-6.6 0L8 11.4V.5M.5 10.8v4.7h15v-4.7"/></svg>
                             </a>
                         </li>
